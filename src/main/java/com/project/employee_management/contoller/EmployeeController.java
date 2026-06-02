1d275bd1e145af166b238cf3754431f153636efa
Iniyan1804 <iniyan.m1842006@gmail.com>
2026-06-02 10:38:53 +0530

    vercel link added

--- src/main/java/com/project/employee_management/contoller/EmployeeController.java
+++ src/main/java/com/project/employee_management/contoller/EmployeeController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/employee")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","https://your-app.vercel.app"})
 public class EmployeeController {
 
     @Autowired
